d804a72d04b21da62499d44ba1529ab305a27b2b
Axel Obscura <axosar@gmail.com>
2015-10-15 18:16:15 -0500

    Intro
    Intro text

--- vet/app/models/receptionist.rb
+++ vet/app/models/receptionist.rb
@@ -1,6 +1,8 @@
 class Receptionist < ActiveRecord::Base
+    belongs_to :user
+    
     validates :date, :length => { :maximum => 1 }
     validates :pet, :length => { :maximum => 1 }
     validates :customer, :length => { :maximum => 1 }
-    validates :date, :length => { :maximum => 1 }
+    validates :reason, :length => { :maximum => 1 }
 end
